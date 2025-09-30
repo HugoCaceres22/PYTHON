a34737d20053cbb89a7d40fc523bc0dcde879e0a
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-09-30 14:38:22 +0200

    Ejercicio11

---
+++ ejercicio11.py
@@ -0,0 +1,11 @@
+Ahorro = float(input("Introduce la cantidad de dinero ahorrado: "))
+A1 = round(Ahorro*1.04, 2)
+A2 = round(A1*1.04, 2)
+A3 = round(A2*1.04, 2)
+
+
+print ("El dinero ahorrado el primer año será : " , A1 , ", el del segundo año: " , A2 , ", y del tercer año: " , A3) 
+
+
+
+
