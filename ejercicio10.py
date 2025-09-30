a4c12a5e91bc9c5bc51bd9f06213dce808114b26
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-09-30 14:31:29 +0200

    Ejercicio10

---
+++ ejercicio10.py
@@ -0,0 +1,9 @@
+NPayaso = float(input("Introduce la cantidad de payasos del pedido: "))
+NMuñeca = float(input("Introduce cantidad de muñecas del pedido: "))
+Peso = NPayaso*122 + NMuñeca*75
+
+print ("El peso total del pedido será: ", Peso ,"g o ", Peso/100 , "KG")
+
+
+
+
