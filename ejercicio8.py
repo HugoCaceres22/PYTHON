5bf1bb014a07f264e052103f1c1da6256c1b0a92
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-09-30 14:15:29 +0200

    Ejercicio8

---
+++ ejercicio8.py
@@ -0,0 +1,14 @@
+n = float(input("Introduce el dividendo: "))
+m = float(input("Introduce el divisor:  "))
+
+c = n//m
+r = n%m 
+
+print(n, "entre", m, "da un cociente", c, "y un resto", r)
+
+
+
+
+
+
+
