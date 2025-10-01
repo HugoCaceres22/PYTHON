dac61b042935195625e3e5396a4688143ae71956
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-10-01 09:46:56 +0200

    Ejercicio12

--- ejercicio12.py
+++ ejercicio12.py
@@ -2,7 +2,7 @@ PA = 3.49
 NB = float(input("Introduce la cantidad de barras vendidas que no son del dia: "))
 DESCUENTO = "60%"
 
-print ("Una barra de pan nomralmente cuesta : " , PA , ", al no ser fresca se le hace un descuento del: " , DESCUENTO , ", por lo que el precio final es  " , round((NB*PA)*0.4, 2)) 
+print ("Una barra de pan normalmente cuesta : " , PA , ", al no ser fresca se le hace un descuento del: " , DESCUENTO , ", por lo que el precio final es  " , round((NB*PA)*0.4, 2)) 
 
 
 
