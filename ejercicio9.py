266877d809a8b9e45231fb4f670fe251bcf490d2
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-10-01 09:59:18 +0200

    Ejercicio 9 mejorado

--- ejercicio9.py
+++ ejercicio9.py
@@ -5,6 +5,6 @@ capital = cantidad * (1 + interes / 100) ** años
 print("El capital obtenido es", capital)
 
 
-
+# L fórmula es Capital Final = 1.000 € x (1 + 0.05)³ = 1.000 € x (1.05)³ = 1.157,63 €.
 
 
