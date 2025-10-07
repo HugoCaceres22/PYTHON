a55a1d632cbc01bf6d577630b2a89dd1177b2756
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-10-07 13:33:49 +0200

    Creación de carpeta y ejercicio 1

--- Primera Relacion/ejercicio8.py
+++ Primera Relacion/ejercicio8.py
@@ -2,7 +2,7 @@ n = float(input("Introduce el dividendo: "))
 m = float(input("Introduce el divisor:  "))
 
 c = n//m
-r = n%m 
+r = n%m #el % es resto
 
 print(n, "entre", m, "da un cociente", c, "y un resto", r)
 
---
+++ cadenas1.py
@@ -0,0 +1,4 @@
+nombre = input("Introduce tu nombre: ")
+veces = int(input("Introduce un número entero: "))
+
+print((nombre + "\n") * veces) #el \n tiene que ir dentro de lo que se va a repeitr
