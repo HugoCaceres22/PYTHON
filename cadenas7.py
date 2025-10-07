eabbfaf447251b166e15a62c840f71293b01b340
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-10-07 14:35:24 +0200

    Ejercicio 7

---
+++ cadenas7.py
@@ -0,0 +1,11 @@
+#Escribir un programa que pregunte el correo electrónico del usuario en la consola y
+#muestre por pantalla otro correo electrónico con el mismo nombre (la parte delante
+#de la arroba @) pero con dominio ceu.es.
+
+correo = input("Introduce tu correo: ")
+
+modcorreo = correo.split("@")[0] + "@ceu.es"
+
+print (f"Tu correo con el dominio cambiado es: {modcorreo}") 
+
+
---
+++ cadenas8.py
@@ -0,0 +1,8 @@
+#Escribir un programa que pregunte por consola el precio de un producto en euros
+#con dos decimales y muestre por pantalla el número de euros y el número de
+#céntimos del precio introducido.
+
+precio = round(float(input("Introduce el precio en euros: ")), 2)
+
+print (f"El precio en euros es: {precio} y el precio en céntimos es: {precio*100}") 
+
