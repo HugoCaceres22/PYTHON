37b3b3cf7f7f76d5bba96bc3f98838c5a024138e
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-10-07 14:35:55 +0200

    Ejercicio 8

--- cadenas8.py
+++ cadenas8.py
@@ -1,6 +1,6 @@
 #Escribir un programa que pregunte por consola el precio de un producto en euros
 #con dos decimales y muestre por pantalla el número de euros y el número de
-#céntimos del precio introducido.
+#céntimos del precio introducido
 
 precio = round(float(input("Introduce el precio en euros: ")), 2)
 
