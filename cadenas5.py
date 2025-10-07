993fce588216b20ee5744ae4b29e2327a8dafbdc
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-10-07 14:13:14 +0200

    Ejercicio 5

---
+++ cadenas5.py
@@ -0,0 +1,13 @@
+frase = input("Introduce una frase: ")
+
+
+frase_invertida = frase[::-1] # donde empieza, donde termina y cada cuantas letras muestra, como es -1 va de derecha a izquierda
+                              # si fuera 2 por ejemplo pondría una letra si una no
+
+
+print(f"Frase invertida: {frase_invertida}")
+
+
+
+
+
