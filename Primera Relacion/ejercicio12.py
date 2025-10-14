04333ab6b9c897ecf78f9b56d3aee5962f514dab
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-10-14 14:06:29 +0200

    Corrección Ejercicio12, Condicionales7

--- Primera Relacion/ejercicio12.py
+++ Primera Relacion/ejercicio12.py
@@ -1,8 +1,8 @@
 PA = 3.49
 NB = float(input("Introduce la cantidad de barras vendidas que no son del dia: "))
-DESCUENTO = "60%"
+DESCUENTO = 60
 
-print ("Una barra de pan normalmente cuesta : " , PA , ", al no ser fresca se le hace un descuento del: " , DESCUENTO , ", por lo que el precio final es  " , round((NB*PA)*0.4, 2)) 
+print ("Una barra de pan normalmente cuesta : " , PA , ", al no ser fresca se le hace un descuento del: " , DESCUENTO , "%, por lo que el precio final es  " , round((NB*PA)*(1-(DESCUENTO/100)), 2)) 
 
 
 
