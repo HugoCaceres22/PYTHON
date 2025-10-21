c58e4d592e842c1ea21004612f58823ffe5551bc
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-10-21 14:14:39 +0200

    Bucles3

---
+++ Bucles/Bucles3.py
@@ -0,0 +1,16 @@
+#Escribir un programa que pida al usuario un número entero positivo y muestre por
+#pantalla todos los números impares desde 1 hasta ese número separados por
+#comas.
+
+
+num = float(input ("Introduce un numero entero positivo: "))
+
+i = 0
+
+while i < num  :
+    i = i + 1
+    if i % 2 != 0 :
+      print (i)
+i = i + 1
+
+     
