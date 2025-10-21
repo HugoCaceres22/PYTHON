05218d30e324b2d6c4831ba6b9d7018a4248ffc3
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-10-21 14:00:29 +0200

    Bucles2

---
+++ Bucles/Bucles2.py
@@ -0,0 +1,12 @@
+#Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos
+#los años que ha cumplido (desde 1 hasta su edad)
+
+
+edad = float(input ("Introduce tu edad: "))
+
+i = edad
+
+while i > 1  :
+    i= i - 1
+    print (i)
+    
