725dda8113e9deead741287c0c75b1132ee2c696
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-10-21 13:54:59 +0200

    Bucles1

---
+++ Bucles/Bucles1.py
@@ -0,0 +1,11 @@
+#Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10
+#veces
+
+palabra = input ("Introduce una aplabara para repetirla 10 veces: ")
+
+i = 0
+
+while i < 10  :
+    print (palabra)
+    i = i + 1
+    
