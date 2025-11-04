8683a1cbea16d1d814cebcdc117be7703625a84a
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-11-04 14:38:54 +0100

    Bucles12 y 7 incompletos

--- Bucles/Bucles7.py
+++ Bucles/Bucles7.py
@@ -1,6 +1,14 @@
 #Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
 
+i = 1
+j = 1
+y = i
 
+while i <= 10 :
+    if j <= 10 :
+      print (y * j)
+      j = j + 1 
+i = i+1
   
   
   
