04332b2af49108c39d5a69a665982313c7d4fccc
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-11-04 13:21:41 +0100

    Bucles6

---
+++ Bucles/Bucles6.py
@@ -0,0 +1,25 @@
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla
+#un triángulo rectángulo como el de más abajo, de altura el número introducido.
+#*
+#**
+#***
+#****
+#*****
+
+
+numero = int(input ("Introduce la altura de la primide : "))
+i = 0
+
+while i <= numero  :
+  print("*" * i)
+  i = i + 1
+  
+  
+  
+  
+
+
+
+     
+
+     
