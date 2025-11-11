38844609bb4e0fcc7c97147fd82e1dce63a8c9c3
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2025-11-11 13:46:54 +0100

    Correccion 11 y 7 terminado

--- Bucles/Bucles11.py
+++ Bucles/Bucles11.py
@@ -1,16 +1,11 @@
 #Escribir un programa que pida al usuario una palabra y luego muestre por pantalla
 #una a una las letras de la palabra introducida empezando por la última.
      
-palabra = str(input("Escribe un numero : "))
+palabra = str(input("Escribe una palabra : "))
 num = len(palabra)
 i = 1
 
 while i <= len(palabra) :
     print (palabra[len(palabra) - i])
     i = i+1
-
-
-
-
-
-        
+      
--- Bucles/Bucles7.py
+++ Bucles/Bucles7.py
@@ -1,14 +1,16 @@
 #Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
 
-i = 1
-j = 1
-y = i
 
-while i <= 10 :
-    if j <= 10 :
-      print (y * j)
-      j = j + 1 
-i = i+1
+
+
+y = 0
+while y < 10 :
+  j = 0
+  y = y + 1
+  while j <= 10 :
+    print (y * j)
+    j = j + 1 
+
   
   
   
