7b7638cbb157517a406c69932a520f44e6384ddb
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2026-01-20 13:35:35 +0100

    Diccionarios10

---
+++ Diccionarios/Diccionarios10.py
@@ -0,0 +1,76 @@
+clientes = {}
+
+while True:
+    print("1. Añadir cliente")
+    print("2. Eliminar cliente")
+    print("3. Mostrar cliente")
+    print("4. Listar todos los clientes")
+    print("5. Listar clientes preferentes")
+    print("6. Terminar")
+
+    opcion = input("Elige una opción: ")
+
+    # 1) Añadir cliente
+    if opcion == "1":
+        nif = input("NIF: ")
+        nombre = input("Nombre: ")
+        direccion = input("Dirección: ")
+        telefono = input("Teléfono: ")
+        correo = input("Correo: ")
+        pref = input("¿Es preferente? (s/n): ")
+
+        preferente = False
+        if pref == "s":
+            preferente = True
+
+        clientes[nif] = {
+            "nombre": nombre,
+            "direccion": direccion,
+            "telefono": telefono,
+            "correo": correo,
+            "preferente": preferente
+        }
+
+        print("Cliente añadido.")
+
+    # 2) Eliminar cliente
+    elif opcion == "2":
+        nif = input("NIF del cliente a eliminar: ")
+
+        if nif in clientes:
+            del clientes[nif]
+            print("Cliente eliminado.")
+        else:
+            print("Ese NIF no existe.")
+
+    # 3) Mostrar cliente
+    elif opcion == "3":
+        nif = input("NIF del cliente a mostrar: ")
+
+        if nif in clientes:
+            print("NIF:", nif)
+            print("Nombre:", clientes[nif]["nombre"])
+            print("Dirección:", clientes[nif]["direccion"])
+            print("Teléfono:", clientes[nif]["telefono"])
+            print("Correo:", clientes[nif]["correo"])
+            print("Preferente:", clientes[nif]["preferente"])
+        else:
+            print("Ese NIF no existe.")
+
+    # 4) Listar todos los clientes
+    elif opcion == "4":
+        for nif in clientes:
+            print(nif, "-", clientes[nif]["nombre"])
+
+    # 5) Listar clientes preferentes
+    elif opcion == "5":
+        for nif in clientes:
+            if clientes[nif]["preferente"] == True:
+                print(nif, "-", clientes[nif]["nombre"])
+
+    # 6) Terminar
+    elif opcion == "6":
+        break
+
+    else:
+        print("Opción no válida.")
