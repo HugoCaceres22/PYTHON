97f083c2065235b064f94ce67e583b2e52070db8
“HugoCaceres22” <hcacmar530@ieszaidinvergeles.org>
2026-01-21 10:14:58 +0100

    11 Incompleto

--- Funciones/funciones11.py
+++ Funciones/funciones11.py
@@ -1,8 +1,14 @@
-frase = "serafin payo serafin"
+def cadena(frase):
+    div = frase.split(" ")
+    dic={}
 
-div = frase.split(" ")
-dic={}
-
-for x in range(len(div)):
-    print(x)
-    print (div[x])
+    for x in range(len(div)):
+        if div[x] in dic:
+                dic[div[x]] =  dic[div[x]] + 1
+                print( dic[div[x]])
+        else:
+            dic[div[x]]=1
+    return dic
+    
+frase = input("Introduce una frase: ")
+print (cadena(frase))
